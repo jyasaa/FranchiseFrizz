6a85fa569dab3c5fff077df7d6b6a9b9b62a3b50
Mourya03 <saimouryacheggoju123@gmail.com>
2021-07-07 19:14:00 +0530

    updated componets, added button, Data is still to be filled

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,28 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { StartComponent } from './start/start.component';
 import { HarrypotterComponent } from './harrypotter/harrypotter.component';
+import { JohnwickComponent } from './johnwick/johnwick.component';
+import { JamesbondComponent } from './jamesbond/jamesbond.component';
+import { JurassicworldComponent } from './jurassicworld/jurassicworld.component';
+import { MissionimpossibleComponent } from './missionimpossible/missionimpossible.component';
+import { PiratesofthecaribbeanComponent } from './piratesofthecaribbean/piratesofthecaribbean.component';
 
 const routes: Routes = [
   {path:"home",component:HomeComponent},
 
   {path:"",component:StartComponent,pathMatch:'full'},
 
-  {path:"harrypotter",component:HarrypotterComponent}
+  {path:"harrypotter",component:HarrypotterComponent},
+
+  {path:"jamesbond",component:JamesbondComponent},
+
+  {path:"johnwick",component:JohnwickComponent},
+
+  {path:"jurassicworld",component:JurassicworldComponent},
+
+  {path:"missionimpossible",component:MissionimpossibleComponent},
+
+  {path:"piratesofthecaribbean",component:PiratesofthecaribbeanComponent}
 ];
 
 @NgModule({
